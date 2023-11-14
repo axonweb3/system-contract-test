572368f4bbb83bd236030d07327d3d1e627303cd
sunchengzhu <sunchengzhu@cryptape.com>
2023-11-14 19:47:17 +0800

    update CkbType.sol

--- test/ckbLightClient.ts
+++ test/ckbLightClient.ts
@@ -1,50 +1,86 @@
 import { ethers } from "hardhat";
 import { Signer } from "ethers";
 import { CkbLightClient } from "../typechain-types";
+import { expect } from "chai";
 
 describe("CkbLightClient", function () {
   let ckbLightClientContract: CkbLightClient;
+  let ckbLightClientContract0: CkbLightClient;
   let signer: Signer;
+  let signer0: Signer;
 
+  const header = {
+    compactTarget: 0x1, // u32
+    dao: Uint8Array.from(Array(32).fill(1)), // [u8; 32]
+    epoch: 1n, // u64
+    number: 0x1n, // u64
+    parentHash: Uint8Array.from(Array(32).fill(1)), // [u8; 32]
+    proposalsHash: Uint8Array.from(Array(32).fill(1)), // [u8; 32]
+    timestamp: 0x1n, // u64
+    transactionsRoot: Uint8Array.from(Array(32).fill(1)), // [u8; 32]
+    version: 0x1, // u32
+    nonce: BigInt("0x1"), // u128
+    extraHash: Uint8Array.from(Array(32).fill(0)), // [u8; 32]
+    extension: new Uint8Array(/* ... */), // Bytes
+    blockHash: Uint8Array.from(Array(32).fill(1)), // [u8; 32]
+  };
 
   beforeEach(async function () {
     const signers: Signer[] = await ethers.getSigners();
     const randomId = Math.ceil(Math.random() * 4);
     signer = signers[randomId];
+    signer0 = signers[0];
 
-    const ckbLightClientContractAddress = "0xffffffffffffffffffffffffffffffffffffff03";
+    const ckbLightClientContractAddress = "0xffffffffffffffffffffffffffffffffffffff02";
     ckbLightClientContract = await ethers.getContractAt("CkbLightClient", ckbLightClientContractAddress, signer);
+    ckbLightClientContract0 = await ethers.getContractAt("CkbLightClient", ckbLightClientContractAddress, signer0);
   });
 
 
-  it("set state", async function () {
-    const tx= await ckbLightClientContract.setState(true);
+  it("set state by validator", async function () {
+    const tx = await ckbLightClientContract.setState(true);
+    expect(tx.hash.length).to.equal(66)
     await tx.wait();
   });
 
-  it("update headers", async function () {
-    const header = {
-      compactTarget: 0x1,
-      dao: Uint8Array.from(new Array(32).fill(1)),
-      epoch: 1n,
-      blockHash: Uint8Array.from(new Array(32).fill(1)),
-      number: 0x1,
-      parentHash: Uint8Array.from(new Array(32).fill(1)),
-      proposalsHash: Uint8Array.from(new Array(32).fill(1)),
-      timestamp: 0x1,
-      transactionsRoot: Uint8Array.from(new Array(32).fill(1)),
-      version: 0x1,
-      nonce: 0x1,
-      extraHash: Uint8Array.from(new Array(32).fill(0)),
-      unclesHash: Uint8Array.from(new Array(32).fill(0)),
-    };
+  it("set state by non-validator", async function () {
+    try {
+      await ckbLightClientContract0.setState(true);
+    } catch (error: unknown) {
+      const errorMessage = (error as Error).message;
+      expect(errorMessage).to.contain("check authorization error")
+    }
+  });
+
+  it("update headers by validator", async function () {
     const tx = await ckbLightClientContract.update([header]);
+    expect(tx.hash.length).to.equal(66)
     await tx.wait();
   });
 
-  it("rollback headers", async function () {
-    const blockHash = "0x" + "5".repeat(64);
+  it("update headers by non-validator", async function () {
+    try {
+      await ckbLightClientContract.update([header]);
+    } catch (error: unknown) {
+      const errorMessage = (error as Error).message;
+      expect(errorMessage).to.contain("check authorization error")
+    }
+  });
+
+  it("rollback headers by validator", async function () {
+    const blockHash = Uint8Array.from(Array(32).fill(1));
     const tx = await ckbLightClientContract.rollback([blockHash]);
+    expect(tx.hash.length).to.equal(66)
     await tx.wait();
+  });
+
+  it("rollback headers by non-validator", async function () {
+    const blockHash = Uint8Array.from(Array(32).fill(1));
+    try {
+      await ckbLightClientContract.rollback([blockHash]);
+    } catch (error: unknown) {
+      const errorMessage = (error as Error).message;
+      expect(errorMessage).to.contain("check authorization error")
+    }
   });
 });
